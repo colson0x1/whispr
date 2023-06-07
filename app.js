ed9744f6690add1fd8320a40f21ca1e3e2f4055a
Colson <colsoncalifornication@gmail.com>
2023-06-07 09:06:53 +0545

    display the message

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+const { hash } = window.location;
+
+const memo = atob(hash.replace('#', ''));
+
+if (memo) {
+  document.querySelector('#memo-form').classList.add('hide');
+  document.querySelector('#memo-show').classList.remove('hide');
+
+  document.querySelector('h1').innerHTML = memo;
+}
+
 document.querySelector('form ').addEventListener('submit', (event) => {
   event.preventDefault();
 
-  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#memo-form').classList.add('hide');
   document.querySelector('#link-form').classList.remove('hide');
 
-  const input = document.querySelector('#message-input');
+  const input = document.querySelector('#memo-input');
   const encrypted = btoa(input.value);
 
   const linkInput = document.querySelector('#link-input');
